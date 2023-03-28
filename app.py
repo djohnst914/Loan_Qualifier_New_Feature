0299ca5bd65dd9715716a4f7801db0a41bcae46b
Dylan <dylanhjjohnston@gmail.com>
2023-03-28 11:04:26 -0700

    import save_csv function

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import fire
 import questionary
 from pathlib import Path
 
+from qualifier.utils.fileio import save_csv
+
 from qualifier.utils.fileio import load_csv
 
 from qualifier.utils.calculators import (
